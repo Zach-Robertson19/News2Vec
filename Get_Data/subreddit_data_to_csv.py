db226b95f9fb6938470d017f0139dc7ba4e83a2e
Zach-Robertson19 <zachrobertson19@gmail.com>
2020-04-09 10:22:57 -0500

    Update all py files
    Update py files to object oriented programs., update .gitignore with *.csv

--- Get_Data/subreddit_data_to_csv.py
+++ Get_Data/subreddit_data_to_csv.py
@@ -5,84 +5,84 @@ import time
 import csv
 import os
 
-''' 
-get_subreddit_titles() is a function that gives you various data for the chosen subreddit
-over a chosen time period, the actual data is specified in the column_names variable
-and can be changed if you know the names of the perameters returned in 
-the request object with is not hard to do given the example here.
+class Get_Data:
 
-get_subreddit_titles() takes 3 arguments
+    def __init__(self, subreddit, start_date, end_date):
+        self.subreddit = subreddit
+        self.start_date = start_date
+        self.end_date = end_date
+        filename = subreddit+ '_data_' +str(start_date)+ \
+            '_' +str(end_date)+ '.csv'
+        self.filename = filename
+        self.check_for_existing_data()
 
-(subreddit) is the name of the subreddit as a string without r/, capitalization matters. E.g. r/politics
-is 'politics'
+    def check_for_existing_data(self):
+        if os.path.isfile(self.filename):
+            df = pd.read_csv(self.filename)
+            last_time = df['created_utc'].iloc[-1]
+            start = pd.Timestamp(str(last_time)).value // 10**9
+            print('file found, adding to data')
+            self.get_subreddit_titles(start, self.end_date)
 
-(start_date, end_date) are the start and end dates in the mm.dd.YYYY format
-'''
+        else:
+            print('No file found, starting new export of data')
+            self.get_subreddit_titles(self.start_date, self.end_date)
 
-def get_subreddit_titles(subreddit, start_date, end_date):
-    start = int(time.mktime(datetime.strptime(start_date, "%m.%d.%Y").timetuple())) 
-    end = int(time.mktime(datetime.strptime(end_date, "%m.%d.%Y").timetuple()))
-
-    filename = subreddit+ '_data_' +str(start_date)+ \
-        '_' +str(end_date)+ '.csv'
-    
-    x = 1
-    if os.path.isfile(filename):
-        df = pd.read_csv(filename)
-        last_time = df['created_utc'].iloc[-1]
-        start = pd.Timestamp(str(last_time)).value // 10**9
-        
-    while True:
-        #difference = end - start
-        '''if difference < 10:
-            print('All Done!')
-            break'''
-  
-        url = 'https://api.pushshift.io/reddit/search/submission/?before='+ str(end)+ '&after=' + str(start) + '&subreddit='+ subreddit
-
-        r = requests.get(url)
-        try:
-            data = r.json()
-        except:
-            print('No data')
-            pass
-        if not data['data']:
-            print('All Done!')
-            break
-        arr = []
-
-        for item in data['data']:
-            column_names = ['created_utc', 'id', 'permalink', \
-                'score', 'selftext', \
-                    'title', 'subreddit', \
-                        'url']
+    def get_subreddit_titles(self, start_date, end_date):
+        subreddit = self.subreddit
+        filename = self.filename
+        start = start_date
+        if isinstance(start_date, str):
+            start = int(time.mktime(datetime.strptime(start_date, "%m.%d.%Y").timetuple())) 
+        end = int(time.mktime(datetime.strptime(end_date, "%m.%d.%Y").timetuple()))
+        x = 1
+        while True:
+            #difference = end - start
+            url = 'https://api.pushshift.io/reddit/search/submission/?before='+ str(end)+ '&after=' + str(start) + '&subreddit='+ subreddit
+            print(url)
+            r = requests.get(url)
             try:
-                arr_new = [pd.to_datetime(item['created_utc'], unit='s'), \
-                    item['id'], item['permalink'], item['score'], \
-                        item['selftext'], item['title'], item['subreddit'], \
-                            item['url']]
+                data = r.json()
             except:
+                print('No data')
                 pass
-                
-            arr.append(arr_new)
-            print(str(x), pd.to_datetime(item['created_utc'], unit='s'), \
-                item['title'])
-            start = item['created_utc']
-            x += 1
-        
-        df = pd.DataFrame(arr)
-        
-        if not os.path.isfile(filename):
-            df.to_csv(filename, header=column_names, index=False)
-        
-        else: 
-            old_csv = pd.read_csv(filename)
-            old_df = pd.DataFrame(old_csv)
-            new_df = df[~df[1].isin(old_df['title'])]
-            if new_df.empty:
-                print('Nothing Added!')
-            new_df.to_csv(filename, mode='a', header=False, index=False)
-        
-        time.sleep(1)
-        
-get_subreddit_titles('politics', '11.21.2018', '03.02.2020')
+            if not data['data']:
+                print('All Done!')
+                break
+            arr = []
+
+            for item in data['data']:
+                column_names = ['created_utc', 'id', 'permalink', \
+                    'score', 'selftext', \
+                        'title', 'subreddit', \
+                            'url']
+                try:
+                    arr_new = [pd.to_datetime(item['created_utc'], unit='s'), \
+                        item['id'], item['permalink'], item['score'], \
+                            item['selftext'], item['title'], item['subreddit'], \
+                                item['url']]
+                except:
+                    pass
+                    
+                arr.append(arr_new)
+                print(str(x), pd.to_datetime(item['created_utc'], unit='s'), \
+                    item['title'])
+                start = item['created_utc']
+                x += 1
+            
+            df = pd.DataFrame(arr)
+            
+            if not os.path.isfile(filename):
+                df.to_csv(filename, header=column_names, index=False)
+            
+            else: 
+                old_csv = pd.read_csv(filename)
+                old_df = pd.DataFrame(old_csv)
+                new_df = df[~df[1].isin(old_df['title'])]
+                if new_df.empty:
+                    print('Nothing Added!')
+                new_df.to_csv(filename, mode='a', header=False, index=False)
+            
+            time.sleep(1)
+
+Get_Data('politics', '11.21.2016', '11.21.2018')
--- unk_set.py
+++ unk_set.py
@@ -10,121 +10,205 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 import warnings 
 import sys 
 import math
+import os
+from langdetect import detect
+from langdetect import DetectorFactory
+import re
+from urllib.parse import urlparse
+import difflib
+from spellchecker import SpellChecker
 
-df = pd.read_pickle('politics_data_11.21.2018_10.10.2019_sent.pkl')
+# Creates a Word2Vec Model and saves it as a bin file
+class Word2Vec_Model:
+    '''
+    Object that creates a Word2Vec Model and saves it as a bin file. Uses the data
+    created in the subreddit_data_to_csv.py file and parses it for punctuation for
+    easier manipulation. The parsed sentences are saved as a pickle file for
+    retrevial later.
 
-def build_set(words, n_words):
-    words = [item for sublist in words for item in sublist]
-    most_occur = [['UNK', -1]]
-    most_occur.extend(Counter(words).most_common(n_words))
-
-
-    dictionary = dict()
-    data = list()
-
-    for word, _ in most_occur:
-        dictionary[word] = len(dictionary)
-
-    unk_words = 0
-    for word in words:
-        if word in dictionary:
-            index = dictionary[word]
+    Parameters:
+        csv_file (str) : csv file created by the subreddit_data_to_csv.py file
+    '''
+    def __init__(self, csv_file):
+        self.csv_filename = csv_file
+        self.model_name = csv_file[:-4] + '_W2VModel'
+        if os.path.isfile(self.model_name + '.bin'):
+            print('Model already exis for this time range')
         else:
-            index = 0
-            unk_words += 1
-        data.append(index)
-    most_occur[0][1] = unk_words
-    reversed_dict = dict(zip(dictionary.values(), dictionary.keys()))
+            print('Model doesn\'t exist, creating model')
+            if os.path.isfile(self.csv_filename[:-4] + '_sent.pkl'):
+                print('Pickle file found')
+                self.df = pd.read_pickle(csv_file[:-4] + '_sent.pkl')
+                self.words = self.df['sent'].to_list()
+            else:
+                self.data = pd.read_csv(csv_file, header=0)
+                self.df = self.word_array(self.data)
+                self.words = self.df['sent'].to_list() 
+            self.create_model(self.words, self.model_name)
 
-    return data, most_occur, dictionary, reversed_dict
+    def create_model(self, words, model_name):
+        data = []
+        s = ' '
+        for item in words:
+            sent = s.join(item)
+            for i in sent_tokenize(sent):
+                temp = []
+                for j in word_tokenize(i):
+                    temp.append(j.lower())
+                data.append(temp)
 
-def TDIDF(sent_words):
-    rawcount_dict = dict(Counter(chain.from_iterable(sent_words)))
-    '''with open('training_sent_dict.pickle', 'wb') as handle:
-        pickle.dump(rawcount_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)'''
-    TF_dict = dict(Counter(chain.from_iterable(set(i) for i in sent_words)))
-    
-    dict_list = list()
-    
-    for sent in sent_words:
-        count_list = list()
-        for elem in sent:
-            count = rawcount_dict[elem]/len(sent)
-            count_list.append(count)
-        sent_dict = dict(zip(sent, count_list))
-        dict_list.append(sent_dict)
-    return dict_list, TF_dict
+        model = Word2Vec(words, min_count=1, size=100, window=5, sg=1)  
+        model.wv.save_word2vec_format(f'{model_name}.bin', binary=True)
+        return model
 
-def create_model(words, model_name):
-    data = []
-    s = ' '
-    for item in words:
-        sent = s.join(item)
-        for i in sent_tokenize(sent):
-            temp = []
-            for j in word_tokenize(i):
-                temp.append(j.lower())
-            data.append(temp)
+    def word_array(self, data, pkl=True):
+        DetectorFactory.seed = 0 
 
-    model = Word2Vec(words, min_count=1, size=100, window=5, sg=1)  
-    model.wv.save_word2vec_format(f'{model_name}.bin', binary=True)
-    return model
+        
+        sentences = pd.DataFrame(columns=['title', 'sent', 'url'])
+        words = 0
 
-def read_model(model_name):
-    model = gensim.models.KeyedVectors.load_word2vec_format(model_name + '.bin', binary=True)
-    return model
+        for i, (title, url) in enumerate(zip(data['title'], data['url'])):
+            try:
+                if detect(title) != 'en':
+                    del df[i]
+            except:
+                pass
+            word_line = []
+            word_line_punct = list(re.split(r'\s+', title))
 
-def TDIDFSentenceVecotrs(model_name, sent_words):
-    model = read_model(model_name)
-    _, TF_dict = TDIDF(sent_words)
-    sent_vectors =[]
-    sentences = []
-    for item in sent_words:
-        weights = []
-        w2v_vectors = []
-        weighted_vectors = []
+            for word in word_line_punct:
+                word_punct = list(filter(None, re.split(r'(^(?:[A-Z]*[a-z]+\'[st])|^(?:[A-Z]*[a-z]+[s]\')|^(?:[A-Z]\.){1,2}|[^a-zA-Z])', word)))
+                for elem in word_punct:
+                    punct_match = re.match(r'[^a-zA-Z0-9]', elem)
+                    if punct_match != None:
+                        pass
+                    else:
+                        word_line.append(elem.lower())
+                        words += 1
+            url = urlparse(url).hostname
+            sentences.at[i, 'title'] = title
+            sentences.at[i, 'sent'] = word_line
+            sentences.at[i, 'url'] = url
+            
 
-        for i in item:
-            word_vector = model[i]
-            weight = TF_dict[i]
-            weights.append(weight)
-            w2v_vectors.append(word_vector)
+        pick_filename = str(self.csv_filename)[:-4] + '_sent.pkl'
+        #pickle the word list
+        if pickle:
+            sentences.to_pickle(pick_filename, protocol=4)
+            print(f'Pickle file added {pick_filename}')
+        return sentences
 
-        for index, j in enumerate(w2v_vectors):
-            TD_vector = weights[index]*j
-            weighted_vectors.append(TD_vector)
+# reads a Word2Vec Model and compares sentences using it
+class Compare_Headlines:
+    '''
+    Object that finds the cosine similarity between the subreddit titles given in
+    the title variable, calculated with the use of the model defined in model_name.
+    These are the models created by the Word2Vec_Model Object.
 
-        sent_vector = np.zeros(shape=TD_vector.shape)
-        for word_vector in weighted_vectors:
-            sent_vector = np.add(sent_vector, word_vector)
-        sent_vectors.append(sent_vector)
-        sentences.append(' '.join(item))
-    sent_vector_dict = dict(zip(sentences, sent_vectors))
-    return sent_vector_dict
+    Parameters:
+        model_name (str) : name of the model you want to use to find the cosine similarity.
+            The titles should not be included in the model
+        
+        titles (list of strings) : The title strings that you want to find the cosine
+            similarity of. If there are more than 2 titles you will get the cosine
+            similarity of all of the groupings.
+    '''
+    def __init__(self, model_name, titles):
+        self.model = self.read_model(model_name)
+        self.pickle_file = model_name[:-9] + '_sent.pkl'
+        self.vectors, self.titles = self.TDIDF_Sentence_Vectors(model_name, titles)
 
-def CosineSimilarity(sent_vector_dict, sent1, sent2) :
-    print(sent1, sent2)
-    vector1 = sent_vector_dict[sent1]
-    vector2 = sent_vector_dict[sent2]
-    print(vector1, vector2)
-    dot_product = np.dot(vector1, vector2)
-    mag1 = np.linalg.norm(vector1)
-    mag2 = np.linalg.norm(vector2)
+    def read_model(self, model_name):
+        model = gensim.models.KeyedVectors.load_word2vec_format(model_name + '.bin', binary=True)
+        return model
 
-    cos_sim = dot_product / (mag1*mag2)
-    return cos_sim
+    def TDIDF_count(self, sent_words):
+        rawcount_dict = dict(Counter(chain.from_iterable(sent_words)))
+        TF_dict = dict(Counter(chain.from_iterable(set(i) for i in sent_words)))
+        
+        dict_list = list()
+        
+        for sent in sent_words:
+            count_list = list()
+            for elem in sent:
+                count = rawcount_dict[elem]/len(sent)
+                count_list.append(count)
+            sent_dict = dict(zip(sent, count_list))
+            dict_list.append(sent_dict)
+        return dict_list, TF_dict
 
+    def TDIDF_Sentence_Vectors(self, model_name, titles):
+        model = self.read_model(model_name)
+        sent_words = pd.read_pickle(self.pickle_file)
+        _, TF_dict = self.TDIDF_count(sent_words['sent'].to_list())
+        sent_vectors =[]
+        sentences = []
+        titles = Eleminate_Punctuation().eleminate_punctuation(titles)['sent'].to_list()
+        
+        
+        for item in titles:
+            weights = []
+            w2v_vectors = []
+            weighted_vectors = []
+            for j, i in enumerate(item):
+                
+                try:
+                    word_vector = model[i]
+                    weight = TF_dict[i]
+                except:
+                    word = list(set(difflib.get_close_matches(i, [item for sublist in sent_words['sent'].to_list() for item in sublist])))
+                    for element in word:
+                        word = SpellChecker().correction(element)
+                    word_vector = model[word]
+                    weight = TF_dict[word]
+                    item[j] = word
+                weights.append(weight)
+                w2v_vectors.append(word_vector)
 
-words = df['sent'].to_list()
+            for index, j in enumerate(w2v_vectors):
+                TD_vector = weights[index]*j
+                weighted_vectors.append(TD_vector)
 
+            sent_vector = np.zeros(shape=TD_vector.shape)
+            for word_vector in weighted_vectors:
+                sent_vector = np.add(sent_vector, word_vector)
+            sent_vectors.append(sent_vector)
+            sentences.append(' '.join(item))
+        sent_vector_dict = dict(zip(sentences, sent_vectors))
+        titles = [' '.join(item) for item in titles]
+        return sent_vector_dict, titles
 
-vector = TDIDFSentenceVecotrs('politics_data_11.21.2018_10.10.2019_W2VModel', words)
+    def CosineSimilarity(self, sent_vector_dict, sent1, sent2):
+        vector1 = sent_vector_dict[sent1]
+        vector2 = sent_vector_dict[sent2]
+        dot_product = np.dot(vector1, vector2)
+        mag1 = np.linalg.norm(vector1)
+        mag2 = np.linalg.norm(vector2)
 
-cos = CosineSimilarity(vector, list(vector.keys())[0], list(vector.keys())[1])
-print(cos)
+        cos_sim = dot_product / (mag1*mag2)
+        return cos_sim
 
+class Eleminate_Punctuation:
 
+    def eleminate_punctuation(self, string_list):
+        DetectorFactory.seed = 0
+        sentences = pd.DataFrame(columns=['sent'])
+        for i, item in enumerate(string_list):
+            word_line = []
+            word_line_punct = list(re.split(r'\s+', item))
 
-#dict_list, TF_dict = TDIDF(words)
-#new_list = sorted(TF_dict, key=TF_dict.get, reverse=True)[:100]
+            for word in word_line_punct:
+                word_punct = list(filter(None, re.split(r'(^(?:[A-Z]*[a-z]+\'[st])|^(?:[A-Z]*[a-z]+[s]\')|^(?:[A-Z]\.){1,2}|[^a-zA-Z])', word)))
+                for elem in word_punct:
+                    punct_match = re.match(r'[^a-zA-Z0-9]', elem)
+                    if punct_match != None:
+                        pass
+                    else:
+                        word_line.append(elem.lower())
+            sentences.at[i, 'sent'] = word_line
+        return sentences
 
+comp = Compare_Headlines('Get_Data/politics_data_11.21.2018_03.02.2020_W2VModel', ['Donal Trump likes anal sex', 'Mike Pence is a queer rights activists'])
+sim = comp.CosineSimilarity(comp.vectors, comp.titles[0], comp.titles[1])
+print(sim)
